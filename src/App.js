3ed6f81334b13d4dcbc2f0819449c584a65b295d
Harmeet Singh <harmeet208singh@gmail.com>
2021-08-20 18:15:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyC0yrmBhp7RL-pRcVufAno8w8TIBQxS2i4",
+  authDomain: "hotel-management-d80be.firebaseapp.com"
+})
+
+
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",  
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <div>
+              You have already booked a room
+            </div>
+            <h3>Your Room Details are as follows:</h3>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+          </span>
+        ) : (
+          <span>
+            <h4>Hostel Allocation</h4>
+            <p>Please Sign in:</p>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </span>
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
